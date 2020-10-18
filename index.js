bab42a65093ec3dc7e0eeff5cd2f9ba2c02e0599
HenryKasai <henrykasai1775@gmail.com>
2020-10-18 02:07:18 -0200

    Exercício em desenvolvimento

---
+++ index.js
@@ -0,0 +1,38 @@
+const button = document.querySelector("button");
+const buttonclick = (e) => {
+    e.preventDefault();
+    let kg = document.getElementById("massa").value;
+    if (Number(kg) && kg > 0 && kg < 800) {
+        let m = document.getElementById("altura").value;
+        if (Number(m) && m > 0.40 && m < 3) {
+            let imc = kg / (m ** m);
+            const div = document.getElementById("resultado");
+            const resultado = () => {
+                div.insertAdjacentHTML("afterbegin", `<p>Seu IMC é de ${imc.toFixed(2)}.</p>`);
+            }
+            if (imc < 17) {
+                resultado();
+                div.insertAdjacentHTML("afterbegin", "<p>Diagnóstico: Muito abaixo do peso.</p>");
+            } if (imc >= 17 && imc <= 18.49) {
+                resultado();
+                div.insertAdjacentHTML("afterbegin", "<p>Diagnóstico: Abaixo do peso.</p>");
+            } if (imc >= 18.5 && imc <= 24.99) {
+                resultado();
+                div.insertAdjacentHTML("afterbegin", "<p>Diagnóstico: Peso normal.</p>");
+            } if (imc >= 25 && imc <= 29.99) {
+                resultado();
+                div.insertAdjacentHTML("afterbegin", "<p>Diagnóstico: Acima do peso.</p>");
+            } if (imc >=  30 && imc <= 34.99) {
+                resultado();
+                div.insertAdjacentHTML("afterbegin", "<p>Diagnóstico: Obesidade I.</p>");
+            } if (imc >= 35 && imc <= 40) {
+                resultado();
+                div.insertAdjacentHTML("afterbegin", "<p>Diagnóstico: Obesidade II (severa).</p>");
+            } if (imc > 40) {   
+                resultado();
+                div.insertAdjacentHTML("afterbegin", "<p>Diagnóstico: Obesidade III (mórbida).</p>");
+            }
+        }
+    }
+}
+button.addEventListener("click", buttonclick);
